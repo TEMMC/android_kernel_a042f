7d7e0973c88dca3d377030de914418d7b90e4caf
TEMMC <mapfumotinotendaenock9@gmail.com>
2026-09-25 10:51:50 +0200

    Fix native Linux kernel config merge

--- build_kernel.sh
+++ build_kernel.sh
@@ -7,56 +7,57 @@ export KBUILD_BUILD_USER="@ravindu644"
 git submodule init && git submodule update
 
 #export toolchain paths
-export BUILD_CROSS_COMPILE="${RDIR}/toolchains/arm-gnu-toolchain-14.2.rel1-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-"
-export BUILD_CC="${RDIR}/toolchains/clang-r383902/bin/clang"
+export BUILD_CROSS_COMPILE="\${RDIR}/toolchains/arm-gnu-toolchain-14.2.rel1-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-"
+export BUILD_CC="\${RDIR}/toolchains/clang-r383902/bin/clang"
 
 #output dir
-if [ ! -d "${RDIR}/out" ]; then
-    mkdir -p "${RDIR}/out"
+if [ ! -d "\${RDIR}/out" ]; then
+    mkdir -p "\${RDIR}/out"
 fi
 
 #build dir
-if [ ! -d "${RDIR}/build" ]; then
-    mkdir -p "${RDIR}/build"
+if [ ! -d "\${RDIR}/build" ]; then
+    mkdir -p "\${RDIR}/build"
 else
-    rm -rf "${RDIR}/build" && mkdir -p "${RDIR}/build"
+    rm -rf "\${RDIR}/build" && mkdir -p "\${RDIR}/build"
 fi
 
 #build options
 export ARGS="
--C $(pwd) \
-O=$(pwd)/out \
--j$(nproc) \
-ARCH=arm64 \
-CROSS_COMPILE=${BUILD_CROSS_COMPILE} \
-CC=${BUILD_CC} \
-CLANG_TRIPLE=aarch64-linux-gnu- \
-KCFLAGS=-w \
-CONFIG_SECTION_MISMATCH_WARN_ONLY=y \
+-C \$(pwd) \\
+O=\$(pwd)/out \\
+-j\$(nproc) \\
+ARCH=arm64 \\
+CROSS_COMPILE=\${BUILD_CROSS_COMPILE} \\
+CC=\${BUILD_CC} \\
+CLANG_TRIPLE=aarch64-linux-gnu- \\
+KCFLAGS=-w \\
+CONFIG_SECTION_MISMATCH_WARN_ONLY=y \\
 "
 
 #build kernel image
 build_kernel(){
-    make ${ARGS} clean && make ${ARGS} mrproper
-    make ${ARGS} a04e_defconfig custom.config
-    make ${ARGS} menuconfig
-    make ${ARGS} || exit 1
-    cp out/arch/arm64/boot/Image.gz $(pwd)/arch/arm64/boot/Image.gz
+    make \${ARGS} clean && make \${ARGS} mrproper
+    make \${ARGS} a04e_defconfig
+    \${RDIR}/scripts/kconfig/merge_config.sh -m "\${RDIR}/out/.config" "\${RDIR}/arch/arm64/configs/custom.config"
+    make \${ARGS} olddefconfig
+    make \${ARGS} || exit 1
+    cp out/arch/arm64/boot/Image.gz "\${RDIR}/arch/arm64/boot/Image.gz"
 }
 
 #build boot.img
-build_boot() {    
-    rm -f ${RDIR}/AIK-Linux/split_img/boot.img-kernel ${RDIR}/AIK-Linux/boot.img
-    cp "${RDIR}/out/arch/arm64/boot/Image.gz" ${RDIR}/AIK-Linux/split_img/boot.img-kernel
-    mkdir -p ${RDIR}/AIK-Linux/ramdisk/{debug_ramdisk,dev,metadata,mnt,proc,second_stage_resources,sys}
-    cd ${RDIR}/AIK-Linux && ./repackimg.sh --nosudo && mv image-new.img ${RDIR}/build/boot.img
+build_boot() {
+    rm -f "\${RDIR}/AIK-Linux/split_img/boot.img-kernel" "\${RDIR}/AIK-Linux/boot.img"
+    cp "\${RDIR}/out/arch/arm64/boot/Image.gz" "\${RDIR}/AIK-Linux/split_img/boot.img-kernel"
+    mkdir -p "\${RDIR}/AIK-Linux/ramdisk/"{debug_ramdisk,dev,metadata,mnt,proc,second_stage_resources,sys}
+    cd "\${RDIR}/AIK-Linux" && ./repackimg.sh --nosudo && mv image-new.img "\${RDIR}/build/boot.img"
 }
 
 #build odin flashable tar
 build_tar(){
-    cd ${RDIR}/build
+    cd "\${RDIR}/build"
     tar -cvf "KernelSU-Next-SM-A042F.tar" boot.img && rm boot.img
-    echo -e "\n[i] Build Finished..!\n" && cd ${RDIR}
+    echo -e "\n[i] Build Finished..!\n" && cd "\${RDIR}"
 }
 
 build_kernel
