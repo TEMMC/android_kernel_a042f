3999be659e37bcd0c0be71f8e6a907a5f1379e23
TEMMC <mapfumotinotendaenock9@gmail.com>
2026-09-25 11:59:21 +0200

    Fix MediaTek ftrace include path

--- kernel/trace/mtk_ftrace.h
+++ kernel/trace/mtk_ftrace.h
@@ -36,7 +36,7 @@ TRACE_EVENT(tracing_on,
 
 #undef TRACE_INCLUDE_PATH
 #undef TRACE_INCLUDE_FILE
-#define TRACE_INCLUDE_PATH .
+#define TRACE_INCLUDE_PATH ../../kernel/trace
 #define TRACE_INCLUDE_FILE mtk_ftrace
 
 /* This part must be outside protection */
